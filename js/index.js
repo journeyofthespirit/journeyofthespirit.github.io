8f59b28c2bb94171ea22b87f5d00201e66115e74
journey_of_the_spirit@yahoo.com <justinthompson593@gmail.com>
2017-12-01 15:51:41 -0800

    Adding javascript

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+				  
+				  
+				  $("#title1").attr("href", "posts/the-dog-and-the-bacon.html");
+				  $("#intro1").load("posts/the-dog-and-the-bacon.html #intro");
+				  
+				  });
