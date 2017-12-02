87f255d6ae8d4e748ab27eda65215b98d3fd685a
journey_of_the_spirit@yahoo.com <justinthompson593@gmail.com>
2017-12-01 16:15:45 -0800

    Added test posts and testing how js works

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
 				  
-				  
+				  $("#title1").load("posts/the-dog-and-the-bacon.html #title");
 				  $("#title1").attr("href", "posts/the-dog-and-the-bacon.html");
 				  $("#intro1").load("posts/the-dog-and-the-bacon.html #intro");
+				  
+				  
+				  $("#title2").load("posts/2.html #title");
+				  $("#title2").attr("href", "posts/2.html");
+				  $("#intro2").load("posts/2.html #intro");
+				  
+				  
+				  $("#title3").load("posts/3.html #title");
+				  $("#title3").attr("href", "posts/3.html");
+				  $("#intro3").load("posts/3.html #intro");
 				  
 				  });
