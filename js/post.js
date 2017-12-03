9d456b1218a545119409c7df163373908a0149ce
journey_of_the_spirit@yahoo.com <justinthompson593@gmail.com>
2017-12-03 13:30:00 -0800

    Lots of changes

---
+++ js/post.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+//				  var thisUrl = window.location.href;     // Returns full URL
+//				  var fbLink = "https://www.facebook.com/sharer/sharer.php?u=" + thisUrl;
+//
+//				  $("#fb_link").attr("href", thisUrl);
+				  
+//				  $("#title1").load("posts/the-dog-and-the-bacon.html #title");
+//				  $("#title1").attr("href", "posts/the-dog-and-the-bacon.html");
+//				  $("#intro1").load("posts/the-dog-and-the-bacon.html #intro");
+//
+//
+//				  $("#title2").load("posts/2.html #title");
+//				  $("#title2").attr("href", "posts/2.html");
+//				  $("#intro2").load("posts/2.html #intro");
+				  
+				  
+				 
+				  
+				  });
